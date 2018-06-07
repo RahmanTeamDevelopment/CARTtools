33ffbb3587a71830e5d7404846c299f7e7c3eb13
Márton Münz <Marton.Muenz@icr.ac.uk>
2018-06-07 20:17:57 +0200

    setup.py updated for cartwriter

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='RahmanTeam',
     author_email='rahmanlab@icr.ac.uk',
     licence='MIT',
-    packages=['main', 'cart2enst_'],
-    scripts=['bin/CART2ENST.py'],
+    packages=['main', 'cart2enst_', 'cartwriter_'],
+    scripts=['bin/CART2ENST.py', 'bin/CARTWriter'],
     zip_safe=False
 )
